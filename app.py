ac1c0986d3449f2c089844de6c026d54ff9395f9
Jan du Plessis <drjanduplessis@icloud.com>
2023-08-26 22:02:42 +0100

    Updated interactive plots

--- app.py
+++ app.py
@@ -1,27 +1,56 @@
 import streamlit as st
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
+from datetime import datetime, timedelta
 
-CSS = """
-h1 {
-    color: #a54b49;
-},
-body {
-    color: #1d4878;
-}
-.stApp {
-    background-image: url(https://a-static.besthdwallpaper.com/simple-minimalism-white-color-wallpaper-2880x1800-82612_8.jpg);
-    background-size: cover;
-}
-"""
+st.title("JP's Currency Tracker")
+st.markdown('Crafted with ♥️ by your favourite **Data Scientist**.')
 
-if st.checkbox('Inject CSS'):
-    st.write(f'<style>{CSS}</style>', unsafe_allow_html=True)
+data = pd.read_csv('https://gist.githubusercontent.com/janduplessis883/e7b834c8293745ddc92b7a1fc57af9c0/raw/currency_rates.csv', parse_dates=['Timestamp'])
 
-st.markdown("# JP's Currency Plots")
-st.markdown('Chart: Base Rate **Euro**')
-
-data = pd.read_csv('https://gist.githubusercontent.com/janduplessis883/e7b834c8293745ddc92b7a1fc57af9c0/raw/477a4e030a07e10bf8a96e562ef590d17605d99a/currency_rates.csv')
-
-option = st.slider('Display no of rows',1,30,1)
+option = st.slider('Display no of rows', 1, 10, 1)
 st.write(data.tail(option))
+
+st.markdown(" EUR | BGP & EUR | USD")
+
+# Dropdown for selecting time range
+time_options = {
+    "Last Week": 7,
+    "Last 2 Weeks": 14,
+    "Last 3 Weeks": 21,
+    "Last 4 Weeks": 28,
+    "Last 6 Weeks": 42,
+    "All": None
+}
+
+selected_time = st.selectbox("Select Time Range", list(time_options.keys()))
+
+# Filter data based on the selected time range
+if selected_time != "All":
+    end_date = data["Timestamp"].max()  # Latest date in the data
+    start_date = end_date - timedelta(days=time_options[selected_time])
+    filtered_data = data[data["Timestamp"].between(start_date, end_date)]
+else:
+    filtered_data = data
+
+# Create the Seaborn line plot for GBP
+fig1, ax1 = plt.subplots(figsize=(15, 3))
+sns.lineplot(data=filtered_data, x='Timestamp', y='GBP', ax=ax1, color='#1d4878')
+ax1.set_title('GBP Exchange Rate for ' + selected_time)
+ax1.set_xlabel('Date')
+ax1.set_ylabel('Rate (GBP)')
+ax1.xaxis.grid(False)  # Remove vertical grid lines
+ax1.yaxis.grid(True)   # Add horizontal grid lines
+st.pyplot(fig1)  # Display the GBP plot in Streamlit
+
+# Create the Seaborn line plot for USD
+fig2, ax2 = plt.subplots(figsize=(15, 3))
+sns.lineplot(data=filtered_data, x='Timestamp', y='USD', ax=ax2, color='#a54b49')
+ax2.set_title('USD Exchange Rate for ' + selected_time)
+ax2.set_xlabel('Date')
+ax2.set_ylabel('Rate (USD)')
+ax2.xaxis.grid(False)  # Remove vertical grid lines
+ax2.yaxis.grid(True)   # Add horizontal grid lines
+st.pyplot(fig2)  # Display the USD plot in Streamlit
+
